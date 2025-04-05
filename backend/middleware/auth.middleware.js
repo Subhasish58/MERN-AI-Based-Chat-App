0298fe633610d685773e11e98c9ada41e59214aa
Subhasish58 <166903452+Subhasish58@users.noreply.github.com>
2025-04-05 14:19:00 +0530

    user context setup and project create endpoint

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import redisClient from "../services/redis.service.js";
 
+
 export const authUser = async (req, res, next) => {
     try {
         const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
@@ -21,11 +22,10 @@ export const authUser = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
-
     } catch (error) {
+
         console.log(error);
 
         res.status(401).send({ error: 'Unauthorized User' });
     }
-
 }
